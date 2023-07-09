891e40098349fcc96690688baa76cb6dea88c4f4
Ben F. Maier <benjaminfrankmaier@gmail.com>
2023-07-09 23:41:49 +0200

    made ready to fill

--- fincoretails/metadata.py
+++ fincoretails/metadata.py
@@ -6,7 +6,7 @@ Contains a bunch of information about this package.
 __version__ = "0.0.0"
 
 __author__ = "Benjamin F. Maier"
-__copyright__ = "Copyright 2021-, " + __author__
+__copyright__ = "Copyright 2023-, " + __author__
 __credits__ = [__author__]
 __license__ = "MIT"
 __maintainer__ = __author__
---
+++ fincoretails/tests/lognormal_tests.py
@@ -0,0 +1,16 @@
+import unittest
+
+import numpy as np
+
+from fincoretails.lognormal import mean
+
+class LognormalTest(unittest.TestCase):
+
+    def test_lognormal(self):
+
+        assert(True)
+
+if __name__ == "__main__":
+
+    T = LognormalTest()
+    T.test_lognormal()
